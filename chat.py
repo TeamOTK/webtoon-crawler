bc6c02bd832cb8285d718ef6fdccd5e0eb489756
sunwookim <sunkim09@g.skku.edu>
2024-01-08 06:51:09 +0900

    FEAT change prompt

--- chat.py
+++ chat.py
@@ -1,5 +1,6 @@
 from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 from langchain.prompts.chat import (
     ChatPromptTemplate,
     SystemMessagePromptTemplate,
@@ -18,65 +19,225 @@ class Chat:
     
     def __init__(self, crawl_list):
         self.crawl_list = crawl_list
+        
+    def titleId_to_title(self, titleId):
+        if titleId == 703844:
+            return '가비지타임'
+        elif titleId == 735661:
+            return '재혼 황후'
+        elif titleId == 769209:
+            return '화산귀환'
     
     def get_csv(self):
-        reply = ""
+        reply = {
+            '가비지타임': {},
+            '재혼 황후': {},
+            '화산귀환': {}
+        }
         for crawl in self.crawl_list:
             week = crawl['week']
             no = crawl['no']
             titleId = crawl['titleId']
             
-            path = f'data/{week}/{no}/{titleId}.csv'
+            allreply_path = f'data/{titleId}/{no}/allreply.csv'
+            bestreply_path = f'data/{titleId}/{no}/bestreply.csv'
             
-            with open(path, 'r', newline='') as f:
+            reply[self.titleId_to_title(titleId)][no] = ''
+            
+            with open(bestreply_path, 'r', newline='') as f:
                 csv_reader = csv.reader(f)
-            
                 header = next(csv_reader)
-                # print(f'헤더: {header}')
-                # 나머지 행은 데이터
                 for row in csv_reader:
-                    reply += row[0] + "\n"
-                    
+                    reply[self.titleId_to_title(titleId)][no] += row[0]
+            
+            # cnt = 0
+            # with open(allreply_path, 'r', newline='') as f:
+            #     csv_reader = csv.reader(f)
+            #     header = next(csv_reader)
+            #     for row in csv_reader:
+            #         reply[self.titleId_to_title(titleId)][no] += row[0]
+            #         cnt += 1
+            #         if cnt == 3:
+            #             break
+        # print(reply)         
         return reply
-    
-    class CommaSeparatedListOutputParser(BaseOutputParser):
-        """LLM 아웃풋에 있는 ','를 분리해서 리턴하는 파서."""
-        def parse(self, text: str):
-            return text.strip().split(", ")
-
-    def parse(self, text: str):
-        return text.strip().split(", ")
             
     def run(self):
         template = """
-        너는 웹툰 작가에게 전달하기 위해 독자들의 댓글을 요약해주는 AI야
-        너는 <{reply}> 를 요약해서 키워드 5개 나열해야 해.
-        이때 각 키워드는 반드시 comma(,)로 분리해서 대답해주고, 이외의 말은 하지 마.
-        질문:"""
+        사용자가 웹툰 내용에 대한 질문을 하면, 무슨 회차인지 사용자에게 설명해줘.
+        데이터는 중첩 딕셔너리로 제공되고, 웹툰의 제목이 키, 그 안의 딕셔너리의 키는 웹툰 회차, 값은 웹툰 회차에 대한 내용이 있어. 
+        내용을 바탕으로 해당 웹툰의 회차를 찾아서 사용자에게 알려줘
+        질문:{question} 데이터:{data}"""
         
-        system_message_prompt = SystemMessagePromptTemplate.from_template(
-            template)
-        human_template = "{text}"
-        human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+        prompt = PromptTemplate.from_template(template)
+        chat_model = ChatOpenAI(
+            # model="gpt-3.5-turbo-1106",
+            model="gpt-3.5-turbo-16k-0613",
+            temperature=0,
+            openai_api_key=OPENAI_API_KEY)
         
-        chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+        message = chat_model.predict(prompt.format(
+            question="청명이가 싸우는 회차가 뭐지?",
+            data=self.get_csv()))
+        print(message)
         
-        chain = LLMChain(
-            llm=ChatOpenAI(openai_api_key = OPENAI_API_KEY),
-            prompt=chat_prompt,
-            output_parser=self.CommaSeparatedListOutputParser()
-        )
-        chain.run()
     
 if __name__ == '__main__':
     
     crawl_list = [
+        # 가비지타입 최근 5화
+        # {
+        #     'week': 'sun',
+        #     'no': 202,
+        #     'titleId': 703844
+        # },
+        # {
+        #     'week': 'sun',
+        #     'no': 201,
+        #     'titleId': 703844
+        # },
+        # {
+        #     'week': 'sun',
+        #     'no': 200,
+        #     'titleId': 703844
+        # },
         {
             'week': 'sun',
-            'no': 202,
+            'no': 199,
             'titleId': 703844
-        }
+        },
+        {
+            'week': 'sun',
+            'no': 198,
+            'titleId': 703844
+        },
+        {
+            'week': 'sun',
+            'no': 197,
+            'titleId': 703844
+        },
+        # {
+        #     'week': 'sun',
+        #     'no': 196,
+        #     'titleId': 703844
+        # },
+        # {
+        #     'week': 'sun',
+        #     'no': 195,
+        #     'titleId': 703844
+        # },
+        # {
+        #     'week': 'sun',
+        #     'no': 194,
+        #     'titleId': 703844
+        # },
+        # {
+        #     'week': 'sun',
+        #     'no': 193,
+        #     'titleId': 703844
+        # },
+        # 재혼 황후 최근 5화
+        # {
+        #     'week': 'fri',
+        #     'no': 167,
+        #     'titleId': 735661
+        # },
+        # {
+        #     'week': 'fri',
+        #     'no': 166,
+        #     'titleId': 735661
+        # },
+        {
+            'week': 'fri',
+            'no': 165,
+            'titleId': 735661
+        },
+        {
+            'week': 'fri',
+            'no': 164,
+            'titleId': 735661
+        },
+        {
+            'week': 'fri',
+            'no': 163,
+            'titleId': 735661
+        },
+        # {
+        #     'week': 'fri',
+        #     'no': 162,
+        #     'titleId': 735661
+        # },
+        # {
+        #     'week': 'fri',
+        #     'no': 161,
+        #     'titleId': 735661
+        # },
+        # {
+        #     'week': 'fri',
+        #     'no': 160,
+        #     'titleId': 735661
+        # },
+        # {
+        #     'week': 'fri',
+        #     'no': 159,
+        #     'titleId': 735661
+        # },
+        # {
+        #     'week': 'fri',
+        #     'no': 158,
+        #     'titleId': 735661
+        # },
+        # 화산 귀환 최근 5화
+        {
+            'week': 'wed',
+            'no': 102,
+            'titleId': 769209
+        },
+        {
+            'week': 'wed',
+            'no': 101,
+            'titleId': 769209
+        },
+        {
+            'week': 'wed',
+            'no': 100,
+            'titleId': 769209
+        },
+        {
+            'week': 'wed',
+            'no': 99,
+            'titleId': 769209
+        },
+        {
+            'week': 'wed',
+            'no': 98,
+            'titleId': 769209
+        },
+        # {
+        #     'week': 'wed',
+        #     'no': 97,
+        #     'titleId': 769209
+        # },
+        # {
+        #     'week': 'wed',
+        #     'no': 96,
+        #     'titleId': 769209
+        # },
+        # {
+        #     'week': 'wed',
+        #     'no': 95,
+        #     'titleId': 769209
+        # },
+        # {
+        #     'week': 'wed',
+        #     'no': 94,
+        #     'titleId': 769209
+        # },
+        # {
+        #     'week': 'wed',
+        #     'no': 93,
+        #     'titleId': 769209
+        # },
     ]
-    
     chat = Chat(crawl_list)
     chat.run()  
--- crawler.py
+++ crawler.py
@@ -24,7 +24,7 @@ options = Options()
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.96 Safari/537.36"
 options.add_argument('user-agent=' + user_agent)
 ## for background
-# options.add_argument("headless") ## 크롤링 창 보이게 하려면 주석 처리
+options.add_argument("headless") ## 크롤링 창 보이게 하려면 주석 처리
 options.add_argument('--window-size=1920, 1080')
 options.add_argument('--no-sandbox')
 options.add_argument("--disable-dev-shm-usage")
@@ -35,7 +35,7 @@ options.add_argument('--disable-blink-features=AutomationControlled')
 class Crawler:
     def __init__(self, save_path, week, no, titleId):
         self.url = f'https://comic.naver.com/webtoon/detail?titleId={titleId}&no={no}&week={week}'
-        self.save_path = save_path + f'{week}/{no}/{titleId}/allreply.csv'
+        self.save_path = save_path + f'{titleId}/{no}/'
     
     def __wait_until_find(self, driver, xpath):
         WebDriverWait(driver, 7).until(EC.presence_of_element_located((By.XPATH, xpath)))
@@ -47,29 +47,33 @@ class Crawler:
         button = driver.find_element(By.XPATH, xpath)
         driver.execute_script("arguments[0].click();", button)
 
-    def get_bestreply(self):
-        driver = webdriver.Chrome(service=Service(), options=options)
-        driver.get(self.url)
-        
-        # //*[@id="cbox_module_wai_u_cbox_content_wrap_tabpanel"]/ul/li[1]/div[1]/div/div[2]/span[2]
-        # //*[@id="cbox_module_wai_u_cbox_content_wrap_tabpanel"]/ul/li[2]/div[1]/div/div[2]/span[2]
-        bestreply_list = []
-        
-        for i in range(1, 16):
-            xpath = f'//*[@id="cbox_module_wai_u_cbox_content_wrap_tabpanel"]/ul/li[{i}]/div[1]/div/div[2]/span[2]'
-            element = self.__wait_until_find(driver, xpath)
-            print(element.text)
-            
-            bestreply_list.append(element.text)
-            
-        self.save_csv(bestreply_list)
-        driver.quit()
+    # def get_bestreply(self):
+    #     driver = webdriver.Chrome(service=Service(), options=options)
+    #     driver.get(self.url)
     
-    def get_allreply(self):
+    #     bestreply_list = []
         
+    #     for i in range(1, 16):
+    #         xpath = f'//*[@id="cbox_module_wai_u_cbox_content_wrap_tabpanel"]/ul/li[{i}]/div[1]/div/div[2]/span[2]'
+    #         element = self.__wait_until_find(driver, xpath)
+    #         print(element.text)
+            
+    #         bestreply_list.append(element.text)
+            
+    #     self.save_csv(bestreply_list)
+    #     driver.quit()
+    
+    def get_reply(self):
         ## Selenium
         driver = webdriver.Chrome(service=Service(), options=options)
         driver.get(self.url)
+        
+        ## BeautifulSoup
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        
+        ## BEST 댓글 저장
+        content_list = soup.find_all('span', attrs={'class': "u_cbox_contents"})
+        self.save_csv("bestreply.csv", content_list)
         
         ## 전체댓글 클릭
         action = ActionChains(driver)
@@ -101,40 +105,178 @@ class Crawler:
         print(len(content_list))
         
         ## csv 저장
-        if not os.path.exists(os.path.dirname(self.save_path)):
-            os.makedirs(os.path.dirname(self.save_path))
-    
-        with open(self.save_path, 'w', newline='') as f:
-            wr = csv.writer(f)
-            wr.writerow(['reply'])
-            
-            for content in content_list:
-                wr.writerow([content.string])
+        self.save_csv("allreply.csv", content_list)
         
         driver.quit()
 
         
-    def save_csv(self, bestreply_list):
-        if not os.path.exists(os.path.dirname(self.save_path)):
-            os.makedirs(os.path.dirname(self.save_path))
+    def save_csv(self, filename, reply_list):
+        if not os.path.exists(os.path.dirname(self.save_path + filename)):
+            os.makedirs(os.path.dirname(self.save_path + filename))
     
-        with open(self.save_path, 'w', newline='') as f:
+        with open(self.save_path+filename, 'w', newline='') as f:
             wr = csv.writer(f)
             wr.writerow(['reply'])
-            for bestreply in bestreply_list:
-                wr.writerow([bestreply])
+            for reply in reply_list:
+                wr.writerow([reply.string])  
     
 if __name__ == '__main__':
     
     crawl_list = [
+        # 가비지타입 최근 10화
         {
             'week': 'sun',
             'no': 202,
             'titleId': 703844
-        }
+        },
+        {
+            'week': 'sun',
+            'no': 201,
+            'titleId': 703844
+        },
+        {
+            'week': 'sun',
+            'no': 200,
+            'titleId': 703844
+        },
+        {
+            'week': 'sun',
+            'no': 199,
+            'titleId': 703844
+        },
+        {
+            'week': 'sun',
+            'no': 198,
+            'titleId': 703844
+        },
+        {
+            'week': 'sun',
+            'no': 197,
+            'titleId': 703844
+        },
+        {
+            'week': 'sun',
+            'no': 196,
+            'titleId': 703844
+        },
+        {
+            'week': 'sun',
+            'no': 195,
+            'titleId': 703844
+        },
+        {
+            'week': 'sun',
+            'no': 194,
+            'titleId': 703844
+        },
+        {
+            'week': 'sun',
+            'no': 193,
+            'titleId': 703844
+        },
+        # 재혼 황후 최근 10화
+        {
+            'week': 'fri',
+            'no': 167,
+            'titleId': 735661
+        },
+        {
+            'week': 'fri',
+            'no': 166,
+            'titleId': 735661
+        },
+        {
+            'week': 'fri',
+            'no': 165,
+            'titleId': 735661
+        },
+        {
+            'week': 'fri',
+            'no': 164,
+            'titleId': 735661
+        },
+        {
+            'week': 'fri',
+            'no': 163,
+            'titleId': 735661
+        },
+        {
+            'week': 'fri',
+            'no': 162,
+            'titleId': 735661
+        },
+        {
+            'week': 'fri',
+            'no': 161,
+            'titleId': 735661
+        },
+        {
+            'week': 'fri',
+            'no': 160,
+            'titleId': 735661
+        },
+        {
+            'week': 'fri',
+            'no': 159,
+            'titleId': 735661
+        },
+        {
+            'week': 'fri',
+            'no': 158,
+            'titleId': 735661
+        },
+        # 화산 귀환 최근 10화
+        {
+            'week': 'wed',
+            'no': 102,
+            'titleId': 769209
+        },
+        {
+            'week': 'wed',
+            'no': 101,
+            'titleId': 769209
+        },
+        {
+            'week': 'wed',
+            'no': 100,
+            'titleId': 769209
+        },
+        {
+            'week': 'wed',
+            'no': 99,
+            'titleId': 769209
+        },
+        {
+            'week': 'wed',
+            'no': 98,
+            'titleId': 769209
+        },
+        {
+            'week': 'wed',
+            'no': 97,
+            'titleId': 769209
+        },
+        {
+            'week': 'wed',
+            'no': 96,
+            'titleId': 769209
+        },
+        {
+            'week': 'wed',
+            'no': 95,
+            'titleId': 769209
+        },
+        {
+            'week': 'wed',
+            'no': 94,
+            'titleId': 769209
+        },
+        {
+            'week': 'wed',
+            'no': 93,
+            'titleId': 769209
+        },
     ]
-    
-    # 'https://comic.naver.com/webtoon/detail?titleId=703844&no=202&week=sun'
     save_path = 'data/'
     
     for crawl in crawl_list:
@@ -144,5 +286,4 @@ if __name__ == '__main__':
 
         crawler = Crawler(save_path, week, no, titleId)
         # crawler.get_bestreply()
-        crawler.get_allreply()
-    
+        crawler.get_reply()
