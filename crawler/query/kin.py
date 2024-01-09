2d10fe1a2b38fd7ed87b290a9156e114824be02d
sunwookim <sunkim09@g.skku.edu>
2024-01-09 20:43:55 +0900

    ADD kin crawler

--- crawler/query/kin.py
+++ crawler/query/kin.py
@@ -23,7 +23,7 @@ options = Options()
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.96 Safari/537.36"
 options.add_argument('user-agent=' + user_agent)
 ## for background
-options.add_argument("headless") ## 크롤링 창 보이게 하려면 주석 처리
+# options.add_argument("headless") ## 크롤링 창 보이게 하려면 주석 처리
 options.add_argument('--window-size=1920, 1080')
 options.add_argument('--no-sandbox')
 options.add_argument("--disable-dev-shm-usage")
@@ -33,7 +33,7 @@ options.add_argument('--disable-blink-features=AutomationControlled')
 
 class Kin:
     def __init__(self, save_path):
-        self.search_url = 'https://kin.naver.com/search/list.naver?query=%EC%9B%B9%ED%88%B0%20%EC%B0%BE%EC%95%84&page='
+        self.search_url = 'https://kin.naver.com/search/list.nhn?sort=none&section=kin&query=%EC%9B%B9%ED%88%B0%20%EC%B0%BE%EC%95%84&period=all&page='
         self.save_path = save_path
     
     def __wait_until_find(self, driver, xpath):
@@ -56,52 +56,64 @@ class Kin:
         soup = BeautifulSoup(driver.page_source, 'lxml')
         
         ## csv 해더 생성
-        if not os.path.exists(os.path.dirname(self.save_path + "kin_old.csv")):
-            os.makedirs(os.path.dirname(self.save_path + "kin_old.csv"))
+        if not os.path.exists(os.path.dirname(self.save_path + "kin_old2.csv")):
+            os.makedirs(os.path.dirname(self.save_path + "kin_old2.csv"))
     
-        with open(self.save_path+"kin_old.csv", 'w', newline='') as f:
+        with open(self.save_path+"kin_old2.csv", 'w', newline='') as f:
             wr = csv.writer(f)
             wr.writerow(['title', 'content', 'answer', 'link'])
         
             ## 전체 데이터 수
-            entire_num = int(soup.select_one('#s_content > div.section > h2 > span > em').string.split('/')[1])
+            entire_num = int(soup.select_one('#s_content > div.section > h2 > span > em').string.split('/')[1].replace(',', ''))
             
             ## 전체 페이지 수
             entire_page = entire_num // 10 + 1
             
             ## 제목, 링크 크롤링
-            for i in tqdm(range(1, entire_page+1)):
+            # for i in tqdm(range(1, entire_page+1), desc='search page'):
+            
+            ## 최대 100까지만 됨..ㅜㅠ
+            for i in tqdm(range(1, 101), desc='search page'):
                 driver.get(self.search_url + str(i))
                 time.sleep(0.2)
                 soup = BeautifulSoup(driver.page_source, 'lxml')
                 
-                title_list = soup.select('#s_content > div.section > ul > li:nth-child(1) > dl > dt > a')
-                
+                title_list = soup.find_all('a', attrs={'class': "_nclicks:kin.txt _searchListTitleAnchor"})
+                # print(len(title_list))
                 ## csv 저장
                 for title in title_list:
-                    wr.writerow([title.string, '', '', title['href']])
-                    
+                    # print(title.get_text())
+                    wr.writerow([title.get_text(), '', '', title['href']])
             
-        with open(self.save_path+"kin_old.csv", 'r', newline='') as f:
+        with open(self.save_path+"kin_old2.csv", 'r', newline='') as f:
             rdr = csv.reader(f)
             
             ## csv 해더 생성
-            if not os.path.exists(os.path.dirname(self.save_path + "kin.csv")):
-                os.makedirs(os.path.dirname(self.save_path + "kin.csv"))
+            if not os.path.exists(os.path.dirname(self.save_path + "kin2.csv")):
+                os.makedirs(os.path.dirname(self.save_path + "kin2.csv"))
             
-            with open(self.save_path+"kin.csv", 'w', newline='') as f2:
+            with open(self.save_path+"kin2.csv", 'w', newline='') as f2:
                 wr = csv.writer(f2)
                 wr.writerow(['title', 'content', 'answer', 'link'])
                 ## 내용, 답변 크롤링
-                for line in rdr:
+                for line in tqdm(rdr, desc='detail page'):
+                    if line[3] == 'link':
+                        continue
                     driver.get(line[3])
                     time.sleep(0.2)
                     soup = BeautifulSoup(driver.page_source, 'lxml')
                     
-                    content = soup.select_one('#content > div.question-content > div > div.c-heading._questionContentsArea.c-heading--default-old > div.c-heading__content')
-                    answer = soup.select_one('#answer_1 > div._endContents.c-heading-answer__content > div._endContentsText.c-heading-answer__content-user > div > div')
-                
-                    wr.writerow([line[0], content, answer, line[3]])
+                    title = soup.find('div', attrs={'class': 'c-heading__title'})
+                    content = soup.find('div', attrs={'class': 'c-heading__content'})
+                    answer = soup.find('div', attrs={'class': 'c-heading-answer__content-user'})
+
+                    #content > div.question-content > div > div.c-heading._questionContentsArea.c-heading--default-old > div.c-heading__title
+                    
+                    title = title.get_text().replace("\n", "").replace("\t", "") if title else ''
+                    content = content.get_text().replace("\n", "").replace("\t", "") if content else ''
+                    answer = answer.get_text().replace("\n", "").replace("\t", "") if answer else ''
+                    
+                    wr.writerow([title, content, answer, line[3]])
                 
     
 if __name__ == '__main__':
