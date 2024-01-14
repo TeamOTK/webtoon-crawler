fe741b448d3bbf88486fc6d0a04a158fbc89077b
sunwookim <sunkim09@g.skku.edu>
2024-01-14 18:31:06 +0900

    FEAT crawl webtoon info

--- src/naver/info.py
+++ src/naver/info.py
@@ -24,9 +24,11 @@ options.add_argument('--disable-blink-features=AutomationControlled')
 
 class Info:
     def __init__(self, save_dir, file_name):
+        self.save_dir = save_dir
+        self.file_name = file_name
         self.url = f'https://comic.naver.com'
         self.driver = Driver().set_driver(options)
-        self.f = Driver().open_file(save_dir=save_dir, file_name=file_name, columns=['id', 'link', 'title', 'author', 'genre', 'description'])
+        self.f = Driver().open_file(save_dir=save_dir, file_name="old_" + file_name, columns=['id', 'link', 'title', 'author', 'genre', 'description'])
 
     def get_webtoon_list(self):
         ## Selenium
@@ -48,7 +50,7 @@ class Info:
             link = webtoon.find('a', attrs={'class': 'Poster__link--sopnC'})['href']
             id = re.sub(r'[^0-9]', '', link)
             
-            print(id, title, link)
+            # print(id, title, link)
             
             webtoon_model.append(Webtoon(
                 id=id,
@@ -65,6 +67,7 @@ class Info:
     def get_webtoon_info(self):
         
         webtoon_model = self.get_webtoon_list()
+        update_webtoon_model = []
         
         for webtoon in tqdm(webtoon_model, desc='webtoon info'):
             ## Selenium
@@ -77,25 +80,38 @@ class Info:
             try:
                 ## 작가
                 author_list = soup.find_all('a', attrs={'class': 'ContentMetaInfo__link--xTtO6'})
-                for author in author_list:
-                    author = author.get_text().replace('\n', ',')
+                author = ""
+                for a in author_list:
+                    author += a.get_text() + ','
+                author = author[:-1]
                 ## 장르
                 genre_list = soup.find_all('a', attrs={'class': 'TagGroup__tag--xu0OH'})
-                for genre in genre_list:
-                    genre = genre.get_text().replace('\n', ',')
+                genre = ""
+                for g in genre_list:
+                    genre += g.get_text().replace('#', '') + ','
+                genre = genre[:-1]
                 ## 설명
                 description = soup.find('p', attrs={'class': 'EpisodeListInfo__summary--Jd1WG'}).get_text()
                 
                 webtoon.author = author
                 webtoon.genre = genre
                 webtoon.description = description
+                
+                # print(webtoon)
+                
+                update_webtoon_model.append(webtoon)
             except:
-                webtoon_model.remove(webtoon)
-            
-        Driver().save_webtoon_csv(webtoon, self.f)
+                print("Error")
+                continue
+        
+        ## csv 저장
+        # print(len(webtoon_model))
+        # print(len(update_webtoon_model))
+        self.f = Driver().open_file(save_dir=self.save_dir, file_name=self.file_name, columns=['id', 'link', 'title', 'author', 'genre', 'description'])
+        Driver().save_webtoon_csv(update_webtoon_model, self.f)
         self.driver.quit()
     
 if __name__ == '__main__':
     info = Info('data/webtoon/', 'info.csv')
-    info.get_webtoon_list()
+    info.get_webtoon_info()
     
